24b8ea77fc7fe8f3d280a3836b72e13993fdfc99
Constantine <Constantine@ConstantineKokkinos.com>
2017-12-07 07:59:12 -0600

    Pester POC problems

---
+++ wtf.tests.ps1
@@ -0,0 +1,10 @@
+﻿import-module Pester
+$var = "string"
+
+Describe "$commandName tests" {
+	InModuleScope Pester {
+		It "Sould pass this value into the scope" {
+				$var | Should Not BeNullOrEmpty
+		}
+	}
+}

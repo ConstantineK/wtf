abf65f2bdf15fba2e5bc7a298cb8bb58cc77a714
Constantine <Constantine@ConstantineKokkinos.com>
2017-12-07 08:01:25 -0600

    added version verification

--- wtf.tests.ps1
+++ wtf.tests.ps1
@@ -3,7 +3,11 @@ $var = "string"
 
 Describe "$commandName tests" {
 	InModuleScope Pester {
-		It "Sould pass this value into the scope" {
+		It "Should have a module version of 4.1.0" {
+			Get-Module Pester | Select-Object -ExpandProperty Version | Should Be "4.1.0"
+		} # Passes
+
+		It "Should pass this value into the scope" {
 				$var | Should Not BeNullOrEmpty
 		}
 	}
